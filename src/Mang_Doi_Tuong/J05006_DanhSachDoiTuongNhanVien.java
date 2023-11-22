b454924608c0d039b17d45783dc50c2ac6b121de
LePhanNhamm <lenham2k3@gmail.com>
2023-11-22 22:48:37 +0700

    init project

--- src/Mang_Doi_Tuong/J05006_DanhSachDoiTuongNhanVien.java
+++ src/Mang_Doi_Tuong/J05006_DanhSachDoiTuongNhanVien.java
@@ -1,9 +1,8 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
-class NhanVien implements Comparable<NhanVien>{
-    private String ma,name,gender,ngaysinh,diachi,maso,ngaykihd,ngay,thang,nam;
+class NhanVien {
+    private String ma,name,gender,ngaysinh,diachi,maso,ngaykihd;
 
     public NhanVien(String ma,String name, String gender, String ngaysinh, String diachi, String maso, String ngaykihd) {
         this.ma = ma;
@@ -13,21 +12,6 @@ class NhanVien implements Comparable<NhanVien>{
         this.diachi = diachi;
         this.maso = maso;
         this.ngaykihd = ngaykihd;
-        String ss[] = ngaysinh.split("/");
-        this.ngay = ss[0];
-        this.thang = ss[1];
-        this.nam = ss[2];
-
-    }
-
-    public int compareTo(NhanVien o1) {
-        if (this.nam.equals(o1.nam)) {
-            if (this.thang.equals(o1.thang)) {
-                return this.ngay.compareTo(o1.ngay);
-            }
-            return this.thang.compareTo(o1.thang);
-        }
-        return this.nam.compareTo(o1.nam);
     }
     public String toString() {
         return this.ma + " " + this.name + " " + this.gender + " " + this.ngaysinh + " " + this.diachi + " " + this.maso + " " + this.ngaykihd;
@@ -43,7 +27,6 @@ public class J05006_DanhSachDoiTuongNhanVien {
             String ma = String.format("%05d",i);
             arr.add(new NhanVien(ma,sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextLine()));
         }
-        Collections.sort(arr);
         for (NhanVien i : arr) {
             System.out.println(i);
         }
